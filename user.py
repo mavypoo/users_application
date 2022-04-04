c3296fb86274c3a5e5eb9393d84c35a677ca3f4b
Maverick Sison <mavericksison@outlook.com>
2022-04-04 01:12:52 -0700

    first commit

--- user.py
+++ user.py
@@ -10,6 +10,9 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    def full(self):
+        return f"{self.first_name} {self.last_name}"
+        
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -26,5 +29,24 @@ class User:
     def save(cls, data):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
-        return connectToMySQL('users_schema').query_db( query, data )
+        results = connectToMySQL('users_schema').query_db( query, data )
+        return results
             # users_schema is the database name
+
+    @classmethod
+    def get_one(cls, data): # Data needed - we need the ID of the specific user
+        query = "SELECT * FROM users WHERE id= %(id)s;"
+        results = connectToMySQL('users_schema').query_db( query, data)
+        return cls(results[0]) # We need a zero here [0] because its the first dictionary
+
+
+    @classmethod 
+    def update(cls, data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s, created_at=NOW(), updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db( query, data )
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id= %(id)s;"
+        return connectToMySQL('users_schema').query_db( query, data )
+
